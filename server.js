3cc531e2d97034adfeacabf7d762a6d250f81f1b
Jesse Caunter <js.caunter@gmail.com>
2021-09-05 12:18:43 +1200

    Set up boilerplate

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const server = express()
+module.exports = server
+
+// Middleware
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+
+// Routes
+server.get('/', (req, res) => {
+    res.render('home')
+})
